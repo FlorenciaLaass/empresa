f9cbf82e36756e759e30db9e8b40039794c543ee
FlorenciaLaass <falaass@hotmail.es>
2022-11-10 13:17:22 -0300

    js

---
+++ tarea2/js/index.js
@@ -0,0 +1,21 @@
+
+console.log ("Buenos dias. Soy el codigo JS del archivo externo del html")
+
+console.log ('Buenos dias. Soy el codigo JS del archivo externo del html');
+
+//Variables
+//Palabras reserverdas
+var nombre;
+
+nombre = "Flor";
+
+console.log ("Mi nombre es: " + nombre);
+
+let apellido = "Laass";
+
+const provincia = "Salta";
+
+nombre = "Pepe";
+apellido = "Sanchez";
+
+console.log ("Mi nombre es ${nombre} y mi apellido es ${apellido}");
