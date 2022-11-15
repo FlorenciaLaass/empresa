309ea3c65498361ded37ee83bf047e51ecc6221b
FlorenciaLaass <falaass@hotmail.es>
2022-11-15 13:10:02 -0300

    js

---
+++ tarea2/js/02.js.js
@@ -0,0 +1,11 @@
+
+console.log("Trabajamos con JavaScript y Node.Js");
+
+//Condicionales
+
+if (true) {
+    console.log("Es true y ejecuto los códigos de las llaves");
+}
+if (false) {
+    console.log("Es false y no ejecuto los códigos de las llaves");
+}
--- tarea2/js/index.js
+++ tarea2/js/index.js
@@ -1,4 +1,3 @@
-
 console.log ("Buenos dias. Soy el codigo JS del archivo externo del html")
 
 console.log ('Buenos dias. Soy el codigo JS del archivo externo del html');
@@ -12,10 +11,38 @@ nombre = "Flor";
 console.log ("Mi nombre es: " + nombre);
 
 let apellido = "Laass";
+console.log ("Mi apellido es: " + apellido)
 
 const provincia = "Salta";
+const DNI = 123456789;
+const PI=3.14;
 
 nombre = "Pepe";
 apellido = "Sanchez";
 
-console.log ("Mi nombre es ${nombre} y mi apellido es ${apellido}");
+// datos de entrada
+
+let persona = prompt (
+    "Ingresa tu nombre: "
+)
+alert("Bienvenido " + persona);
+
+let numero = parseInt (prompt ("Ingresa un numero: "));
+let numeroDos = parseInt (prompt ("Ingresa un numero: "));
+
+let resultado = numero + numeroDos;
+alert ('El resultado de a suma es: ${resultado}')
+
+let a = 100;
+let b = 150;
+console.log(a + b);
+
+let usuario = prompt ("Ingresa tu usuario: ");
+let pass =  parseInt (prompt ("Ingresa tu password numérico: "));
+
+if( usuario == "admin" && pass == 1234){
+alert("Bienvenido Administrador")
+} else {
+    alert("usuario o password incorrectos")
+}
+
