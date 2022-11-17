e783a3c02abbb990d3519aa6d399743d8e397bd0
FlorenciaLaass <falaass@hotmail.es>
2022-11-17 13:14:23 -0300

    mas js

--- tarea2/js/02.js.js
+++ tarea2/js/02.js.js
@@ -1,11 +1,16 @@
 
 console.log("Trabajamos con JavaScript y Node.Js");
+console.log("Trabajamos con JavaScript y Node.Js");
+console.log("Trabajamos con JavaScript y Node.Js");
+console.log("Trabajamos con JavaScript y Node.Js");
+
+
 
 //Condicionales
 
 if (true) {
     console.log("Es true y ejecuto los códigos de las llaves");
 }
-if (false){
+if (false) {
     console.log("Es falso y no ejecuto los códigos de las llaves");
 }
